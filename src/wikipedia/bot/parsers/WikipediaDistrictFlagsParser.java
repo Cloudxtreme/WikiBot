e80d96e4ac63109bea60962525397b3bae26d2a2
Myroslav Rudnytskyi <miroslav.rudnitsky@gmail.com>
2014-06-07 16:10:40 +0300

    Bug fix
    Correct work for some wiki articles with names like "Першотравневий"

--- src/wikipedia/bot/parsers/WikipediaDistrictFlagsParser.java
+++ src/wikipedia/bot/parsers/WikipediaDistrictFlagsParser.java
@@ -24,7 +24,7 @@ public class WikipediaDistrictFlagsParser extends SiteParser {
 	protected Map<String, String> getParams(String text, String[] selectors)
 		throws ApplicationException {
 			Map<String, String> map = new HashMap<String, String>();
-			int index = text.indexOf("ий райо́н — ");
+			int index = text.indexOf("ий райо́н");
 			if (index != -1) {
 				StringBuilder sb = new StringBuilder();
 				char current = text.charAt(index--);
